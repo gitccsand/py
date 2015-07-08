cee94162f34a376e5cf03e80a5bf4b67f32baf6b
gitccsand <gozilla@163.com>
2015-07-08 17:15:38 +0800

    day2

---
+++ Coroutine_test.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+'Coroutine'
+
+__author__='lhw'
+
+import pdb
+
+def consumer():
+    r = ''
+    while True:
+        pdb.set_trace()
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+##        n=n-1
+
+def produce(c):
+##    pdb.set_trace()
+    c.send(None)
+    m = 0
+    while m < 5:
+##        pdb.set_trace()
+        m = m + 1
+        print('[PRODUCER] Producing %s...' % m)
+        r = c.send(m)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+c = consumer()
+produce(c)
+
+if __name__=='__main__':
+    pass
+##    main()
+    
+
+
+
+    
+    
+
+
+    
+
+        
+        
+    
+        
+        
