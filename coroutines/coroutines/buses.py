53b12e9d95288283f5692b93042c27ee54c3bf35
gitccsand <jl_post@163.com>
2015-10-10 21:44:42 +0800

    add 1 bus filter & correct queue module name

--- coroutines/coroutines/buses.py
+++ coroutines/coroutines/buses.py
@@ -50,8 +50,9 @@ if __name__ == '__main__':
     xml.sax.parse("allroutes.xml",
                   EventHandler(
                    buses_to_dicts(
+                       filter_on_field("route","22",
                        filter_on_field("direction","North Bound",
-                   bus_locations()))
+                   bus_locations())))
                    ))
                    
 ##              EventHandler(
