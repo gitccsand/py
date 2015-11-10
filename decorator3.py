397ca382572abb20ca2ceed192070f5399ee5645
gitccsand <gozilla@163.com>
2015-11-10 16:52:52 +0800

    20151110

--- decorator3.py
+++ decorator3.py
@@ -13,3 +13,22 @@ def now():
 
 
 now()
+
+def co_log(func):
+        def deco(*args,**kw):
+                def wrapper(*args,**kw):
+                        print('start call %s():' % func.__name__)
+                        func(*args,**kw)
+                        print('end call %s():'%func.__name__)
+                        return wrapper
+        return deco
+
+@co_log
+def now():
+        print(time.time())
+
+now()
+
+        
+
+                
